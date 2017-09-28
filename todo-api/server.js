ee45bf9e0ba604bea11d51379a7f38b6242e01a9
tarun konda <tarunkonda2016@gmail.com>
2017-09-28 18:27:35 +0530

    hello

---
+++ todo-api/server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000 ;
+
+app.get('/',function(req,res){
+	res.send('todo api');
+});
+
+app.listen(port,function(){
+
+	console.log('listening'+port);
+
+});
